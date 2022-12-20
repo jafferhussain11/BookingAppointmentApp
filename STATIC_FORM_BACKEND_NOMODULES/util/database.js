5f065959aad95584db77ba3f719965ca8e41a30e
jafferhussain11 <43640220+jafferhussain11@users.noreply.github.com>
2022-12-20 18:51:45 +0530

    Update database.js

--- STATIC_FORM_BACKEND_NOMODULES/util/database.js
+++ STATIC_FORM_BACKEND_NOMODULES/util/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 
-const sequelize = new Sequelize('BookingApp', 'root', 'Maria123#', {
+const sequelize = new Sequelize(DBADMINPASS', {
 
     dialect: 'mysql',
     host : 'localhost',
